92e127bb16267c7633cb7e2de160d44b6da81f49
csgaikwad <csgaikwad321@gmail.com>
2024-04-09 23:56:49 +0530

    minor changes

--- src/components/Verify.jsx
+++ src/components/Verify.jsx
@@ -5,7 +5,7 @@ import Footer from './subComponents/Footer'
 
 export default function VerifyEmail() {
   return (
-    <div className='min-h-screen w-screen '>
+    <div className='min-h-screen w-screen sm:w-[79rem] '>
       <Navbar/>
       <EmailVerification/>
       <Footer/>
